7ae1c5833058ccf74acacd84a984858b33e36721
aysenurcaglar <aysenurcaglar24@gmail.com>
2024-03-12 23:38:11 +0300

    still haven't figured out the issue

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,8 +6,8 @@ export const Profile = () => {
     const { apiResponse } = useLanguage();
 
     const apiProfile = apiResponse.profile;
-    const info = apiProfile.basicInformation;
-    const about = apiProfile.aboutMe;
+    const apiInfo = apiProfile.basicInformation;
+    const apiAbout = apiProfile.aboutMe;
 
     if (!apiResponse || Object.keys(apiResponse).length === 0) {
         // Handle loading state (e.g., show loading spinner)
@@ -19,26 +19,26 @@ export const Profile = () => {
             <h2 className="font-bold text-black-heading dark:text-bluish-gray text-4xl mb-4">{apiProfile?.title}</h2>
             <div className="flex flex-col gap-x-4 lg:gap-x-8 lg:flex-row">
                 <div className="pb-8 lg:pb-16">
-                    <h3 className="text-dark-purple dark:text-dark-mode-name text-2xl font-medium">{info?.title}</h3>
+                    <h3 className="text-dark-purple dark:text-dark-mode-name text-2xl font-medium">{apiInfo?.title}</h3>
                     <div className="flex flex-row place-content-between">
                         <div className="max-w-xs flex flex-col text-black-heading dark:text-white font-bold mr-4 lg:mr-8">
-                            <p className="my-4">{info?.items[0].label}</p>
-                            <p className="my-4">{info?.items[1].label}</p>
-                            <p className="my-4">{info?.items[2].label}</p>
-                            <p className="my-4">{info?.items[3].label}</p>
+                            <p className="my-4">{apiInfo?.items[0].label}</p>
+                            <p className="my-4">{apiInfo?.items[1].label}</p>
+                            <p className="my-4">{apiInfo?.items[2].label}</p>
+                            <p className="my-4">{apiInfo?.items[3].label}</p>
                         </div>
                         <div className="max-w-xs flex flex-col text-black-heading dark:text-white mr-4 lg:mr-8">
-                            <p className="my-4">{info?.items[0].value}</p>
-                            <p className="my-4">{info?.items[1].value}</p>
-                            <p className="my-4">{info?.items[2].value}</p>
-                            <p className="my-4">{info?.items[3].value}</p>
+                            <p className="my-4">{apiInfo?.items[0].value}</p>
+                            <p className="my-4">{apiInfo?.items[1].value}</p>
+                            <p className="my-4">{apiInfo?.items[2].value}</p>
+                            <p className="my-4">{apiInfo?.items[3].value}</p>
                         </div>
                     </div>
                 </div>
                 <div className="max-w-lg pb-8 lg:pb-16">
-                    <h3 className="text-dark-purple dark:text-dark-mode-name text-2xl mb-4 font-medium">{about?.title}</h3>
-                    <p className="text-gray dark:text-white mb-4">{about?.paragraphs[0]}</p>
-                    <p className="text-gray dark:text-white">{about?.paragraphs[1]}</p>
+                    <h3 className="text-dark-purple dark:text-dark-mode-name text-2xl mb-4 font-medium">{apiAbout?.title}</h3>
+                    <p className="text-gray dark:text-white mb-4">{apiAbout?.paragraphs[0]}</p>
+                    <p className="text-gray dark:text-white">{apiAbout?.paragraphs[1]}</p>
                 </div>
             </div>
             <div className="h-[1px] bg-lilac mb-8 lg:mb-16"></div>
