8a5fbaf26c5f8ebc5399409d8bbb81e684b6ffa9
aysenurcaglar <aysenurcaglar24@gmail.com>
2024-03-14 08:06:09 +0300

    cleaning up the code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,15 @@ import { useLanguage } from "../contexts/LanguageContext";
 
 export const Footer = () => {
 
-  const { apiResponse } = useLanguage();
+  const { apiResponse, language } = useLanguage();
   const { contactHeader, contactEmail, personalBlog, github, linkedin, links } = apiResponse?.footer;
+
+  const isTurkish = language === 'tr';
 
   return (
     <footer id="footer" className="bg-gray-light dark:bg-dark-footer-bg w-full">
       <div className="max-w-[80%] xl:max-w-6xl m-auto py-16">
-        <h2 className="font-bold text-black-heading dark:text-bluish-gray text-left text-4xl max-w-md">{contactHeader}</h2>
+        <h2 className={`font-bold text-black-heading dark:text-bluish-gray text-left text-4xl ${isTurkish ? 'max-w-lg' : 'max-w-md'}`}>{contactHeader}</h2>
         <div className="flex flex-col lg:flex-row pt-16 pb-8">
           <a className="text-mail-red dark:text-lilac text-left mb-16 lg:mb-0 mr-auto" href={`mailto:${contactEmail}`}>👉 <span className="underline">{contactEmail}</span></a>
           <nav className="flex items-center space-x-4 lg:space-x-8 ml-auto">
--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -30,12 +30,12 @@ export const Introduction = () => {
                 <nav className="flex flex-wrap place-content-evenly xl:place-content-between max-w-md mt-8">
                     <button
                         onClick={scrollToFooter}
-                        className={'text-white bg-navy-blue m-2 xl:m-0 h-14 pt-3 px-4 lg:pl-4 w-32 lg:w-36 border border-navy-blue rounded-md dark:text-black-heading dark:bg-dark-mode-hire'}
+                        className={'text-white bg-navy-blue m-2 xl:m-0 h-14 pt-3 px-4 lg:pl-4 w-32 xl:w-36 border border-navy-blue rounded-md dark:text-black-heading dark:bg-dark-mode-hire'}
                     >
                         {hireMe}
                     </button>
-                    <a href={links.github} className={'text-navy-blue m-2 xl:m-0 pt-4 px-4 h-14 lg:pl-6 w-32 lg:w-36 border border-navy-blue rounded-md dark:text-dark-mode-hire dark:border-dark-mode-hire dark:bg-dark-gray-button'}>{darkMode ? (<img src={githubDark} className="inline mr-2" />) : (<img src={githubLight} className="inline mr-2" />)}{github}</a>
-                    <a href={links.linkedin} className={'inline-flex text-navy-blue m-2 xl:m-0 pt-4 px-4 h-14 lg:pl-6 w-30 lg:w-36 border border-navy-blue rounded-md dark:text-dark-mode-hire dark:border-dark-mode-hire dark:bg-dark-gray-button'}>{darkMode ? (<img src={LinkedInDark} className="inline-flex mr-2 mb-4" />) : (<img src={LinkedInLight} className="inline-flex mr-2 mb-4" />)}{linkedin}</a>
+                    <a href={links.github} className={'text-navy-blue m-2 xl:m-0 pt-4 px-4 h-14 lg:pl-6 w-32 xl:w-36 border border-navy-blue rounded-md dark:text-dark-mode-hire dark:border-dark-mode-hire dark:bg-dark-gray-button'}>{darkMode ? (<img src={githubDark} className="inline mr-2" />) : (<img src={githubLight} className="inline mr-2" />)}{github}</a>
+                    <a href={links.linkedin} className={'inline-flex text-navy-blue m-2 xl:m-0 pt-4 px-4 h-14 lg:pl-6 w-30 xl:w-36 border border-navy-blue rounded-md dark:text-dark-mode-hire dark:border-dark-mode-hire dark:bg-dark-gray-button'}>{darkMode ? (<img src={LinkedInDark} className="inline-flex mr-2 mb-4" />) : (<img src={LinkedInLight} className="inline-flex mr-2 mb-4" />)}{linkedin}</a>
                 </nav>
             </div>
             <img className="rounded-2xl mt-8 lg:mt-0 max-w-[476px] max-h-[375px]" src="../image1.jpg" />
